ab4a6164f65e7eccce3c90aaeaed01f333b68f5d
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-10-04 20:19:32 +0300

    Сделал модели блюд

--- foodplan/models.py
+++ foodplan/models.py
@@ -1,2 +1,24 @@
 from django.db import models
 
+
+class Dish(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='image', blank=True)
+    calories = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.title}{self.description}{self.image}{self.calories}'
+
+
+class Ingridients(models.Model):
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    product = models.CharField(max_length=200)
+    amount = models.IntegerField(blank=True, null=True)
+
+
+class DishType(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
