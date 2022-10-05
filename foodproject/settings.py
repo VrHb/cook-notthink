a1307e244f96ddd7d85a97cbf4edcf758c359fc0
Victor Abdukarimov <viredmi@gmail.com>
2022-10-05 09:37:44 +0300

    Added bot app and default_auto_field

--- foodproject/settings.py
+++ foodproject/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'foodplan.apps.FoodplanConfig'
+    
+    'foodplan'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
