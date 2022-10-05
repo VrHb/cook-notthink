595c170309e3b183094fdc1ddc9d1860a545b3dd
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-10-05 22:46:27 +0300

    make parser to json

--- parser.py
+++ parser.py
@@ -1,34 +1,57 @@
 import requests
+import collections
+import pprint
+import json
 from bs4 import BeautifulSoup
-import argparse
+from urllib.parse import urljoin
+from pathvalidate import sanitize_filename
+collections.Callable = collections.abc.Callable
 
 
-
-def get_coock_recipe(url):
-	response = requests.get(url)
-	response.raise_for_status()
-	soup = BeautifulSoup(response.text, 'html.parser')
-	header = soup.find_all("div", {"class": "header"})
-	return header
-
-
-def parse_recipe(recipe_id):
-	url = f"https://www.iamcook.ru{recipe_id}"
-	response = requests.get(url)
-	response.raise_for_status()
-	soup = BeautifulSoup(response.text, 'html.parser')
-	return soup
+def parse_recipe(url, pages, dish_type):
+	recipes_per_page = 30
+	recipes ={
+				  "dishtype": dish_type,	
+				  "title": "",
+				  "description": "",
+				  "ingredients": "",
+				  "imgs_url": "",
+				  "calories": "",
+		}
+	for page in range(1, pages+1):
+		recipes_url = f"{url}/{page}"
+		response = requests.get(recipes_url)
+		response.raise_for_status()
+		soup = BeautifulSoup(response.text, 'html.parser')
+		titles_soup = soup.find_all("div", class_="header")
+		titles = [title.text for title in titles_soup]
+		descriptions_soup = soup.find_all("div", class_="description")
+		descriptions = [sanitize_filename(description.text) for description in descriptions_soup]
+		ingredients_soup = soup.find_all("div", class_="ingredients")
+		ingredients = [(ingredient.text).replace("\n", " ") for ingredient in ingredients_soup]
+		imgs_url_soup = soup.find_all("img", {"itemprop": "image"})
+		imgs_url = [img["src"].replace("//", "") for img in imgs_url_soup]
+		calories_soup = soup.find_all("div", class_="energy tt")
+		calories = [amount.text for amount in calories_soup]
+		for element in range(len(titles)-1):
+			recipes["dishtype"] = dish_type
+			recipes["title"] = titles[element]
+			recipes["description"] = descriptions[element]
+			recipes["ingredients"] = ingredients[element]
+			recipes["imgs_url"] = imgs_url[element]
+			recipes["calories"] = calories[element]
+		with open('recipes.json', 'w') as fp:
+			json.dump(recipes, fp)
 
 
 def main():
-	parser = argparse.ArgumentParser(description="")
-	parser.add_argument('start_id', type=int,
-                        help='ID of the recipe to start parsing from')
-	parser.add_argument('end_id', type=int,
-                        help='ID of the last recipe to parse')
-	args = parser.parse_args()
-	start_args = args.start_id
-	end_args = args.end_id
+	pages = 4
+	vegetaian_url = "https://www.iamcook.ru/event/everyday/everyday-vegetarian"
+	nonglyuten_url = 'https://www.iamcook.ru/event/baking/gluten-free-baking'
+	meat_url = "https://www.iamcook.ru/showsubsection/myasnie_bluda"
+	parse_recipe(vegetaian_url, pages, dish_type="Вегетарианская")
+	parse_recipe(nonglyuten_url, pages, dish_type="Безглютеновая")
+	parse_recipe(meat_url, pages, dish_type="Мясные блюда")
 
 
 if __name__ == "__main__":
