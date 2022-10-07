979fd1023e8e6f9fd06ef2aa3464eb06cdee935e
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-10-07 18:49:59 +0300

    fix models and make parser

--- foodplan/models.py
+++ foodplan/models.py
@@ -5,23 +5,12 @@ class Dish(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='image', blank=True)
-    calories = models.IntegerField(blank=True, null=True)
+    calories = models.CharField(max_length=200, blank=True, null=True)
+    dishtype = models.CharField(max_length=200, blank=True, null=True)
+    ingridients = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return f'{self.title}{self.description}{self.image}{self.calories}'
-
-
-class Ingridients(models.Model):
-    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
-    product = models.CharField(max_length=200)
-    amount = models.IntegerField(blank=True, null=True)
-
-
-class DishType(models.Model):
-    title = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.title
+        return f'{self.title}{self.description}{self.image}{self.calories}{self.dishtype}{self.ingridients}'
 
 
 class User(models.Model):
