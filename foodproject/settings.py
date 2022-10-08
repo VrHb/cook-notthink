67cdbbf4e7fa3b15960426eac00e9184d21682d2
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-10-08 22:18:07 +0300

    Update settings.py

--- foodproject/settings.py
+++ foodproject/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'foodplan.apps.FoodplanConfig',
-    'foodplan',
+    'import-export',
 ]
 
 MIDDLEWARE = [
